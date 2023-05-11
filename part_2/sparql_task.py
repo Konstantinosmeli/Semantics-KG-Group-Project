387080f99e4d4c1e8cf0f7206b7adb5e495e335c
Konstantinos Meli <49761743+Konstantinosmeli@users.noreply.github.com>
2023-05-11 23:05:33 +0300

    new testing with queries for task 2

--- part_2/sparql_task.py
+++ part_2/sparql_task.py
@@ -19,20 +19,39 @@ class SparqlQuery:
         print(f'Result: {result} Length: {len(result)}')
         for row in result:
             print(row)
-        # result.serialize(destination=f'{output_file}_results.csv',format='csv')
+        result.serialize(destination=f'{output_file}_results.csv',format='csv')
 
 if __name__ == "__main__":
-    filename = 'part_2/pizza-restaurants-model-ontology/pizza-restaurants-ontology_with_data.ttl'
+    filename = 'part_2/pizza-ontology/pizza_restaurants_with_reasoning_sparql1.ttl'
 
     sparql_query = SparqlQuery(filename)
 
     def task1():
         query = '''
-        SELECT DISTINCT ?state
+        SELECT ?name ?address ?city ?state ?postcode ?country
         WHERE {
-            ?Restaurant cw:locatedInState 'TX' .
+            ?restaurant fp:name ?name .
+            ?restaurant fp:address ?address .
+            ?restaurant fp:city ?city .
+            ?restaurant fp:state ?state .
+            ?restaurant fp:postcode ?postcode .
+            ?restaurant fp:country ?country
+            FILTER regex(?state, "TX", "i")
         }
         '''
         sparql_query.make_query_to_csv(query,'SPARQL1_subtask')
 
-    task1()
+    def task2():
+        query = '''
+        SELECT ?city (AVG(?menu_item_price) AS ?avg_value)
+        WHERE {
+            ?restaurant fp:name ?city .
+            ?restaurant fp:menu_item ?menu_item .
+            ?menu_item fp:menu_item_price ?menu_item_price .
+            FILTER (?menu_item_price > 10)
+        }
+        GROUP BY ?city
+        '''
+        sparql_query.make_query_to_csv(query,'SPARQL2_subtask')
+
+    task2()
