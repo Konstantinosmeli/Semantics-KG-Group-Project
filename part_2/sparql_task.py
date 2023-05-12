9a9d6fc50ad7b160f507299fcfafa0c413f9cd22
Konstantinos Meli <49761743+Konstantinosmeli@users.noreply.github.com>
2023-05-12 23:20:24 +0300

    task 2 queries are done

--- part_2/sparql_task.py
+++ part_2/sparql_task.py
@@ -29,26 +29,102 @@ if __name__ == "__main__":
     sparql_query = SparqlQuery(filename)
             
     def task1():
-        query = '''
-        SELECT DISTINCT ?name ?address ?city ?state ?postcode ?country
+        query = ''' 
+        SELECT ?name ?firstLineAddress ?cityName  ?stateName
         WHERE {
-           ?x cw:typeRestaurant ?name .
+            ?restaurant cw:name ?name .
+            ?restaurant a cw:Restaurant .
+            ?restaurant cw:locatedAddress ?address .
+            ?address cw:firstLineAddress ?firstLineAddress .
+            ?address cw:locatedCity ?city .
+            ?city cw:name ?cityName .
+            ?address cw:locatedState ?state .
+            ?state cw:name ?stateName .
+            FILTER (?stateName = "TX")
         }
         '''
         sparql_query.make_query_to_csv(query,'SPARQL1.1_subtask')
 
     def task2():
         query = '''
-        SELECT ?city (AVG(?menu_item_price) AS ?avg_value)
+        SELECT ?restaurantName (AVG(?menu_item_price) AS ?avg_value)
         WHERE {
-            ?restaurant fp:name ?city .
-            ?restaurant fp:menu_item ?menu_item .
-            ?menu_item fp:menu_item_price ?menu_item_price .
-            FILTER (?menu_item_price > 10)
+            ?menuItem cw:servedInRestaurant ?restaurant .
+            ?menuItem cw:hasValue ?value .
+            ?value cw:amount ?menu_item_price .
+            ?restaurant a cw:Restaurant .
+            ?restaurant cw:name ?restaurantName .
+            FILTER (?restaurantName = "Burgers & Cupcakes")
         }
-        GROUP BY ?city
         '''
         sparql_query.make_query_to_csv(query,'SPARQL2_subtask')
+
+    def task3():
+        query = '''
+        SELECT ?cityName (COUNT(?restaurant) AS ?num_restaurants)
+        WHERE {
+                ?restaurant cw:locatedAddress ?address .
+                ?address cw:firstLineAddress ?firstLineAddress .
+                ?address cw:locatedCity ?city .
+                ?address cw:locatedState ?state .
+                ?state cw:name ?stateName .
+                ?city cw:name ?cityName .
+                FILTER (?stateName != "WA")
+        }
+        GROUP BY ?cityName
+        HAVING (COUNT(?restaurant))
+        '''
+        sparql_query.make_query_to_csv(query,'SPARQL3_subtask')
+
+    def task4():
+        query = '''
+        SELECT ?cityName (COUNT(?restaurant) AS ?num_restaurants) (AVG(?menu_item_price) AS ?avg_value)
+        WHERE {
+            ?restaurant cw:locatedAddress ?address .
+            ?address cw:firstLineAddress ?firstLineAddress .
+            ?address cw:locatedCity ?city .
+            ?city cw:name ?cityName .
+            ?menuItem cw:servedInRestaurant ?restaurant .
+            ?menuItem cw:hasValue ?value .
+            ?value cw:amount ?menu_item_price .
+        }
+        GROUP BY ?cityName
+        HAVING (COUNT(?restaurant) > 7 && AVG(?menu_item_price) > 10)
+        ORDER BY DESC(?num_restaurants) ?avg_value
+        '''
+        sparql_query.make_query_to_csv(query,'SPARQL4_subtask')
+
+    def task5():
+        query = '''
+        SELECT ?name ?cityName ?menu_item_price
+        WHERE {
+            {
+                ?restaurant cw:name ?name .
+                ?restaurant a cw:Restaurant .
+                ?restaurant cw:locatedAddress ?address .
+                ?address cw:firstLineAddress ?firstLineAddress .
+                ?address cw:locatedCity ?city .
+                ?city cw:name "New York".
+            } UNION {
+                ?restaurant cw:name ?name .
+                ?restaurant a cw:Restaurant .
+                ?restaurant cw:locatedAddress ?address .
+                ?address cw:firstLineAddress ?firstLineAddress .
+                ?address cw:locatedCity ?city .
+                ?menuItem cw:servedInRestaurant ?restaurant .
+                ?menuItem cw:hasValue ?value .
+                ?value cw:amount ?menu_item_price .
+                ?city cw:name ?cityName .
+                FILTER NOT EXISTS {
+                    ?menuItem cw:servedInRestaurant ?restaurant .
+                    ?menuItem cw:hasValue ?value .
+                    ?value cw:amount ?menu_item_price .
+                    FILTER (?menu_item_price > 5)
+                }
+            }
+        }
+        '''
+        sparql_query.make_query_to_csv(query,'SPARQL5_subtask')
 
     def print_predicates():
         predicates = set(sparql_query.graph.predicates())
@@ -64,4 +140,4 @@ if __name__ == "__main__":
         #     print(f"Prefix: {prefix}, Namespace: {namespace}")
 
     #print_predicates()
-    task1()
+    task5()
