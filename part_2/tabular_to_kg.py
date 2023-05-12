0327439600edb113b25646865be0a3d9acdb6ace
qhuy0801 <qhuy0168@gmail.com>
2023-05-12 16:52:38 +0100

    :sparkles: Upload everything again :sparkles:

--- part_2/tabular_to_kg.py
+++ part_2/tabular_to_kg.py
@@ -14,16 +14,6 @@ if __name__ == '__main__':
                        _name_space_prefix=NAMESPACE_PREFIX)
 
     pizza_kg.convert_csv_to_rdf()
-    pizza_kg.save_graph("test1.ttl")
-    # pizza_kg.perform_reasoning(ontology=ONTOLOGY)
-    # pizza_kg.save_graph("pizza_restaurant_reasoned.ttl")
-    # array = "Prosciutto and roasted red peppers topped with aged white cheddar, mozzarella, parmesan and arugula, drizzled with balsamic glaze"
-    # noises = ["and", "/", "&"]
-    # element_noise = ["Restaurant", "restaurant"]
-    # meaningful_noise = {
-    #     "and": ",",
-    #     "or": ",",
-    #     "with": ","
-    # }
-    #
-    # print(pizza_kg.preprocessing_array_string(array, noises, element_noise, meaningful_noise))
+    pizza_kg.save_graph("pizza_restaurant.ttl")
+    pizza_kg.perform_reasoning(ontology=ONTOLOGY)
+    pizza_kg.save_graph("pizza_restaurant_reasoned.ttl")
